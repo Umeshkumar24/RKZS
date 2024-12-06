f7963887338353a3e62efdce97bd3ec0712b3225
UMESH KUMAR DAS <101031787+Umeshkumar24@users.noreply.github.com>
2024-12-06 18:40:20 +0530

    added component
    done

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 import Logo_c from "../Logo_c";
+import { GlareCard } from "../ui/glare-card";
 
 export const HoverEffect = ({
   items,
@@ -50,11 +51,28 @@ export const HoverEffect = ({
             )}
           </AnimatePresence>
           <Card>
+          <GlareCard className="flex flex-col items-center justify-center">
+          <svg
+            width="66"
+            height="65"
+            viewBox="0 0 66 65"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7 text-white"
+          >
+            <path
+              d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+              stroke="currentColor"
+              strokeWidth="15"
+              strokeMiterlimit="3.86874"
+              strokeLinecap="round"
+              />
+            </svg>
             <Logo_c />
             <center><CardTitle>{item.title}</CardTitle></center>
             <CardDescription>{item.description}</CardDescription>
+            </GlareCard>
           </Card>
-          
         </Link>
       ))}
     </div>
