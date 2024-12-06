69d202aaa7e913a0f606856b9bb41d904c66e030
UMESH KUMAR DAS <101031787+Umeshkumar24@users.noreply.github.com>
2024-12-06 19:43:15 +0530

    done
    done

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import { WorldMap1 } from "@/components/WorldMap1";
 
 export default function Home() {
   return (
     <>
        <WorldMap1 />
+       <BackgroundBeams />
     </>
   );
 }
